843b4474611a96894a5df795d02d3fa19a2d6027
Andruss Trockiy <trockiy@gmail.com>
2017-05-13 19:23:11 +0300

    Initial commit

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+	# print(filepath)
+	fjson = open(filepath + "source.json","r")
+	# data= fjson.read()
+	return json.loads(fjson.read())
+
+    
 
 
 def pretty_print_json(data):
-    pass
+	print(json.dumps(data, indent=4, sort_keys=True,ensure_ascii=False))
+	# pprint.pprint(data)
+    
+
+def main():
+	# print('Please provide directory of JSON files :')
+	# print('i.e /home/userdev/\n')
+	# script, filename = argv
+	dirpath = sys.argv[1]
+	print('The directory is :{}'.format(dirpath))
+
+	#dirpath = input('Directory:')
+	jdata = load_data(filepath=dirpath)
+	#print(jdata)
+	pretty_print_json(data=jdata)
 
 
 if __name__ == '__main__':
-    pass
+	main()
+
+
+	# i.e dirpath = '/home/adnruss/mydev/'
+    
