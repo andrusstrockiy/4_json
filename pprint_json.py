9acee3e5d1c42e24a856dd2555d63cb3f3201280
Andruss Trockiy <trockiy@gmail.com>
2017-05-16 00:01:38 +0300

    remarks items 2 3 4 6

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import json
 import sys
 
 
 def load_data(filepath):
-	fjson = open(filepath,"r")
-	return json.loads(fjson.read())
+    fjson = open(filepath,"r")
+    return json.loads(fjson.read())
 
-    
 
-def pretty_print_json(data):
-	print(json.dumps(data, indent=4, sort_keys=True,ensure_ascii=False))
-	# pprint.pprint(data)
-    
+def pretty_print_json(data): 
+    print(json.dumps(data, indent=4, sort_keys=True,ensure_ascii=False))
 
 def main():
-	dirpath = sys.argv[1]
-	print('The directory is :{}'.format(dirpath))
-	jdata = load_data(filepath=dirpath)
-	pretty_print_json(data=jdata)
+    dirpath = sys.argv[1]
+    print('The directory is :{}'.format(dirpath))
+    jdata = load_data(filepath=dirpath) 
+    pretty_print_json(data=jdata)
 
 
-if __name__ == '__main__':
-	main()
-    
+if __name__ == '__main__': 
+    main()
