c718cdc0f4d70224959a016a89873d4e039a79f5
Andruss Trockiy <trockiy@gmail.com>
2017-05-16 00:12:20 +0300

    Remarks 1 5

--- pprint_json.py
+++ pprint_json.py
@@ -11,10 +11,14 @@ def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True,ensure_ascii=False))
 
 def main():
-    dirpath = sys.argv[1]
-    print('The directory is :{}'.format(dirpath))
-    jdata = load_data(filepath=dirpath) 
-    pretty_print_json(data=jdata)
+    if len(sys.argv) == 1:
+        print("Please provide path to json file ")
+        sys.exit(1)
+    else:
+        dirpath = sys.argv[1]
+        print('The directory is :{}'.format(dirpath))
+        jdata = load_data(filepath=dirpath) 
+        pretty_print_json(data=jdata)
 
 
 if __name__ == '__main__': 
